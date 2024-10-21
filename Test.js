ee9c29a0974ab651c8c926a31458c10789a956a9
Abhishek Wathore <abhishekbwathore@gmail.com>
2024-10-21 17:05:57 +0530

    js codes

---
+++ Test.js
@@ -0,0 +1,37 @@
+console.log("Hello World");
+
+var x = 100;
+if(x > 10){
+    var a = 99;
+    console.log("x is greater than 10");
+}
+
+a = 12;
+console.log(a);
+
+
+var name="Abhishek";
+var id= 10;
+var cond = true;
+var w = 'a';
+var t = null;
+
+console.table([name,id,cond,w,t]);
+
+
+
+let num = 100;
+console.log(num);
+
+if(num == 100){
+    let m = 90;
+
+}
+console.log();
+
+// const k = true;
+
+// k=false;
+// console.log(k);
+
+
